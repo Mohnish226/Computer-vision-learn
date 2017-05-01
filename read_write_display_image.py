bdaf64728db0738031296ff546ffc2ea423c2f47
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-05-01 13:15:53 +0530

    Read, write and display images

---
+++ read_write_display_image.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 12:57:31 2017
+
+@author: Mohnish_Devadiga
+"""
+# We don't need to do this again, but it's a good habit
+import cv2 
+import numpy as np
+
+# Load an image using 'imread' specifying the path to image
+input = cv2.imread('./images/input.jpg')
+
+print 'Height of Image:', int(input.shape[0]), 'pixels'
+print 'Width of Image: ', int(input.shape[1]), 'pixels'
+# Our file 'input.jpg' is now loaded and stored in python 
+
+# To display our image variable, we use 'imshow'
+# The first parameter will be title shown on image window
+# The second parameter is the image varialbe
+
+
+#cv2.imshow('Hello World', input)
+
+# Simply use 'imwrite' specificing the file name and the image to be saved
+cv2.imwrite('./images/Output/output.jpg', input)
+cv2.imwrite('./images/Output/output.png', input)
+
+# 'waitKey' allows us to input information when a image window is open
+
+cv2.waitKey()
+
+# This closes all open windows 
+# Failure to place this will cause your program to hang
+
+cv2.destroyAllWindows()
