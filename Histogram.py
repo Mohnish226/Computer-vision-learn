646cc8bd01d893577bbac9629c34345e3ad13668
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-05-03 21:02:09 +0530

    Histogram

--- Histogram.py
+++ Histogram.py
@@ -13,13 +13,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # For First Image
+print "First Image"
 
 image = cv2.imread('images/input.jpg')
 
 histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
 
 # We plot a histogram, ravel() flatens our image array 
-plt.hist(image.ravel(), 256, [0, 256]); plt.show()
+plt.hist(image.ravel(), 256, [0, 256]); 
+plt.show()
 
 # Viewing Separate Color Channels
 color = ('b', 'g', 'r')
@@ -33,13 +35,15 @@ for i, col in enumerate(color):
 plt.show()
 
 # For Second Image
+print "Second Image"
 
 image2 = cv2.imread('images/tobago.jpg')
 
 histogram = cv2.calcHist([image2], [0], None, [256], [0, 256])
 
 # We plot a histogram, ravel() flatens our image array 
-plt.hist(image2.ravel(), 256, [0, 256]); plt.show()
+plt.hist(image2.ravel(), 256, [0, 256]); 
+plt.show()
 
 # Viewing Separate Color Channels
 color = ('b', 'g', 'r')
