dc89c9b46944145fbf624d4b01f277849852312c
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-05-04 22:18:17 +0530

    Simple Drawings

--- Draw.py
+++ Draw.py
@@ -9,7 +9,7 @@ Created on Wed May  3 21:43:38 2017
 import cv2
 import numpy as np
 
-'''
+
 # Create a black image
 image = np.zeros((512,512,3), np.uint8)
 
@@ -40,12 +40,35 @@ cv2.rectangle(image, (100,100), (300,250), (127,50,127), -1)
 cv2.imshow("Rectangle", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
+
 
 # Draw a Circle
 image = np.zeros((512,512,3), np.uint8)
 
 cv2.circle(image, (350, 350), 100, (15,75,50), -1) 
 cv2.imshow("Circle", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Draw Polygon
+
+image = np.zeros((512,512,3), np.uint8)
+
+# Let's define four points
+pts = np.array( [[10,50], [400,50], [90,200], [50,500]], np.int32)
+
+# Let's now reshape our points in form  required by polylines
+pts = pts.reshape((-1,1,2))
+
+cv2.polylines(image, [pts], True, (0,0,255), 3)
+cv2.imshow("Polygon", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Adding Text
+image = np.zeros((512,512,3), np.uint8)
+
+cv2.putText(image, 'Hello World!', (75,290), cv2.FONT_HERSHEY_COMPLEX, 2, (100,170,0), 3)
+cv2.imshow("Hello World!", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
