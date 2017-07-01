8cb7ea03b2164e03c0564f4bbca43bfb4ddd900d
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-07-01 21:42:41 +0530

    Color Filter

---
+++ Color_Filter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 21:38:16 2017
+
+@author: Mohnish_Devadiga
+"""
+
+import cv2
+import numpy as np
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+
+# define range of PURPLE color in HSV
+lower_purple = np.array([125,0,0])
+upper_purple = np.array([175,255,255])
+
+# loop until break statement is exectured
+while True:
+    
+    # Read webcam image
+    ret, frame = cap.read()
+    
+    # Convert image from RBG/BGR to HSV so we easily filter
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+    # Use inRange to capture only the values between lower & upper_blue
+    mask = cv2.inRange(hsv_img, lower_purple, upper_purple)
+
+    # Perform Bitwise AND on mask and our original frame
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('Original', frame)  
+    cv2.imshow('mask', mask)
+    cv2.imshow('Filtered Color Only', res)
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
